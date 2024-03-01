f53febe381038525ca9ad384ee54f64ae6b0d0a6
Ali Zahid <alizahid98@live.co.uk>
2024-03-01 16:50:23 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["an aspiring developer", "a self-taught developer", "passionate and motivated"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+
+})  
