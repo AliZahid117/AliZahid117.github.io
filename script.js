c498c2d751a23c820a549d95c30fbfa96adba4d4
Ali Zahid <alizahid98@live.co.uk>
2024-03-10 19:54:32 +0000

    Updated text strings in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["an aspiring developer", "a self-taught developer", "passionate and motivated"],
+    strings: ["an aspiring developer.", "a problem solver.", "passionate and motivated."],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
